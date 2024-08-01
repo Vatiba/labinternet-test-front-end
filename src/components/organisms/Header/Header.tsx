f74805e85f525b7c8731288eef52aa7f04e13dcf
Vatiba <anonimanonimus955@gmail.com>
2024-08-01 20:40:01 +0500

    :sparkles: feat: Header completed

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import Container from 'components/atoms/Container';
+import Drawer from 'components/molecules/Drawer';
+import BurgerButton from 'components/molecules/BurgerButton';
+import Text from 'components/atoms/Text';
+import clsx from 'clsx';
 import styles from './Header.module.scss';
 
 // icons
 import LogoIcon from 'assets/icons/logo.svg';
-import Text from 'components/atoms/Text';
-import Drawer from 'components/molecules/Drawer';
-import BurgerButton from 'components/molecules/BurgerButton';
+import RighIcon from 'assets/icons/right.svg';
 
 const Header = () => {
    const [isOpened, setIsOpened] = useState(false);
@@ -17,7 +19,9 @@ const Header = () => {
             <Container className={styles.header__inner}>
                <div className={styles.header__logoWrapper}>
                   <img className={styles.header__logo} src={LogoIcon} alt="Logo" />
-                  <Text.H5 className={styles.header__logoTxt}>
+                  <Text.H5 className={clsx(styles.header__logoTxt, {
+                     [styles.header__logoTxt_black]: isOpened
+                  })}>
                      testLab
                   </Text.H5>
                </div>
@@ -47,8 +51,36 @@ const Header = () => {
             anchor='right'
             isOpened={isOpened}
             onClose={() => setIsOpened(false)}
+            drawerStyle={{ width: '100vw' }}
          >
-            hello
+            <Container>
+               <nav className={styles.drawer}>
+                  <a className={styles.drawer__item} href='#'>
+                     <span className={styles.drawer__itemTxt}>
+                        Как это работает
+                     </span>
+                     <img src={RighIcon} alt="Go" />
+                  </a>
+                  <a className={styles.drawer__item} href='#'>
+                     <span className={styles.drawer__itemTxt}>
+                        3-й блок
+                     </span>
+                     <img src={RighIcon} alt="Go" />
+                  </a>
+                  <a className={styles.drawer__item} href='#'>
+                     <span className={styles.drawer__itemTxt}>
+                        Вопросы и ответы
+                     </span>
+                     <img src={RighIcon} alt="Go" />
+                  </a>
+                  <a className={styles.drawer__item} href='#'>
+                     <span className={styles.drawer__itemTxt}>
+                        Форма
+                     </span>
+                     <img src={RighIcon} alt="Go" />
+                  </a>
+               </nav>
+            </Container>
          </Drawer>
       </>
    )
