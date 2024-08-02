9a45e7cac0b77aac4c4b50b349b92fa6a1d83ee0
Vatiba <anonimanonimus955@gmail.com>
2024-08-02 18:02:02 +0500

    :sparkles: feat: CommonInfo secton completed

--- src/templates/CommonInfo/CommonInfo.tsx
+++ src/templates/CommonInfo/CommonInfo.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React from 'react';
+import styles from './CommonInfo.module.scss';
+import Container from 'components/atoms/Container';
+import Text from 'components/atoms/Text';
 
 function CommonInfo() {
    return (
-      <div>CommonInfo</div>
+      <section className={styles.info}>
+         <Container>
+            <div className={styles.row}>
+               <div className={styles.col}>
+                  <div className={styles.info__item}>
+                     <Text.body18 className={styles.info__title} fontWeight='bold'>
+                        Опрошенных пользовались микрокредитами
+                     </Text.body18>
+                     <Text.body16 className={styles.info__text}>
+                        Не могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах Не могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах
+                     </Text.body16>
+                  </div>
+               </div>
+               <div className={styles.col}>
+                  <div className={styles.info__item}>
+                     <Text.body18 className={styles.info__title} fontWeight='bold'>
+                        Опрошенных пользовались микрокредитами
+                     </Text.body18>
+                     <Text.body16 className={styles.info__text}>
+                        Не могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах Не могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафахНе могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах
+                     </Text.body16>
+                  </div>
+               </div>
+               <div className={styles.col}>
+                  <div className={styles.info__item}>
+                     <Text.body18 className={styles.info__title} fontWeight='bold'>
+                        Опрошенных пользовались микрокредитами
+                     </Text.body18>
+                     <Text.body16 className={styles.info__text}>
+                        Не могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах Не могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафахНе могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах
+                     </Text.body16>
+                  </div>
+               </div>
+               <div className={styles.col}>
+                  <div className={styles.info__item}>
+                     <Text.body18 className={styles.info__title} fontWeight='bold'>
+                        Опрошенных пользовались микрокредитами
+                     </Text.body18>
+                     <Text.body16 className={styles.info__text}>
+                        Не могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах Не могли покрыть внезапные траты свыше 7000 ₽. Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах
+                     </Text.body16>
+                  </div>
+               </div>
+            </div>
+         </Container>
+      </section>
    )
 }
 
